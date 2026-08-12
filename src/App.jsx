c45fcea263f102c53d21805d1f15d0f8a0f9ff4e
Boss <vapehood74@gmail.com>
2026-08-12 16:47:50 +0700

    sing up

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function MainShopSystem({ showAdmin }) {
   const prevMonth = new Date(now.getFullYear(), now.getMonth() - 1, 1);
   const prevMonthName = prevMonth.toLocaleString('th-TH', { month: 'long' });
 
-  const categories = ["Sings five 20k","Marbo9000", "Marbo 10k", "Relx go smash 12k", "Relx novo 14k", "Relx Spartar 20k", "Relx Creator 20k", "Relx Creator clear 18k", "Infy 20k", "M switch 15k","Marbo 25k","Esko bar 20k","Lambo 12k"];
+  const categories =["Sings five 20k","Marbo9000", "Marbo 10k", "Relx go smash 12k", "Relx novo 14k", "Relx Spartar 20k", "Relx Creator 20k", "Relx Creator clear 18k", "Infy 20k", "M switch 15k","Marbo 25k","Esko bar 20k","Lambo 12k"];
 
   useEffect(() => { fetchData(); }, []);
 
